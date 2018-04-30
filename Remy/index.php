21fe315991fb8263868a370ffb5c507efe582890
exilon2400 <exilon2400@hotmail.com>
2018-05-01 00:01:36 +0200

    test "modifier"

--- Remy/index.php
+++ Remy/index.php
@@ -1,7 +1,11 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Roboto" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="http://exilon2400.ovh/api/velocity.js"></script>
+	<title>Test</title>
 </head>
 <body>
 
