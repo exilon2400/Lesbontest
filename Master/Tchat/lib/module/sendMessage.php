6aafbaadc93bdb93520edb0e312c7599261921b2
exilon2400 <exilon2400@hotmail.com>
2018-05-01 08:07:45 +0200

    REOFF

--- Master/Tchat/lib/module/sendMessage.php
+++ Master/Tchat/lib/module/sendMessage.php
@@ -1,6 +1,6 @@
 <?php
 	include '../master/init.php';
 	if (isset($_GET["message"]) && isset($_GET["user"]) && isset($_GET["timestamp"])) {
-		$bdd->exec("INSERT INTO `messages`(`owner`, `message`, `pubDate`) VALUES (`".htmlspecialchars($_GET["user"])."`,`".htmlspecialchars($_GET["message"])."`,`".htmlspecialchars($_GET["timestamp"])."`)");
+		$bdd->exec('INSERT INTO `messages`(`owner`, `message`, `pubDate`) VALUES ("'.htmlspecialchars($_GET["user"]).'","'.htmlspecialchars($_GET["message"]).'","'.htmlspecialchars($_GET["timestamp"]).'")');
 	}
 ?>
