a75f0012cca75a32cab3105e9e8ca6749ab688b2
exilon2400 <exilon2400@hotmail.com>
2018-05-01 08:05:02 +0200

    Correction pour les appos

--- Master/Tchat/lib/module/clearMessage.php
+++ Master/Tchat/lib/module/clearMessage.php
@@ -4,7 +4,7 @@
 		$bdd->exec("TRUNCATE messages");
 		echo "clear";
 		$message = htmlspecialchars($_GET["user"])." à vider la conversation à ".date('d/m/Y à G:i', (htmlspecialchars($_GET["timestamp"])+(3600*2)));
-		$bdd->exec("INSERT INTO `messages`(`owner`, `message`, `pubDate`) VALUES ('Server','".htmlspecialchars($message)."','".htmlspecialchars($_GET["timestamp"])."')");
+		$bdd->exec("INSERT INTO `messages`(`owner`, `message`, `pubDate`) VALUES ('Server', '".htmlspecialchars($message)."','".htmlspecialchars($_GET["timestamp"])."')");
 		header("Location: ../../dev.php?pseudo=".htmlspecialchars($_GET["user"]));
 	} else {
 		header("Location: ../../dev.php?pseudo=".htmlspecialchars($_GET["user"]));
